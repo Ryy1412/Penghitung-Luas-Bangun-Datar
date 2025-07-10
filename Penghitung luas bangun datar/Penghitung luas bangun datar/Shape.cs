c47db26d36b78c0dfe9051484c55ea01fc644b10
Rayyis Hammam Mukhayyar_5405 <rayyishammam@gmail.com>
2025-07-10 12:28:33 +0700

    Add files via upload

---
+++ Penghitung luas bangun datar/Penghitung luas bangun datar/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penghitung_luas_bangun_datar
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+}
---
+++ Penghitung luas bangun datar/Penghitung luas bangun datar/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penghitung_luas_bangun_datar
+{
+    public class Triangle : Shape
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double @base, double height)
+        {
+            Base = @base;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+
+}
