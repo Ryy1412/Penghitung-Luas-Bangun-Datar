8026dea80748f7ceb13c35cf2b113def763a0a8c
Rayyis Hammam Mukhayyar_5405 <rayyishammam@gmail.com>
2025-07-10 12:33:07 +0700

    Add files via upload

---
+++ Penghitung luas bangun datar/Penghitung luas bangun datar/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penghitung_luas_bangun_datar
+{
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+
+}
